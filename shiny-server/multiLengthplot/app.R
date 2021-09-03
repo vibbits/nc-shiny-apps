5831b11e9a7f304b04de55370da85d11932f1539
Stephane Plaisance <stephane.plaisance@vib.be>
2021-09-03 11:32:15 +0200

    Update app.R

--- shiny-server/multiLengthplot/app.R
+++ shiny-server/multiLengthplot/app.R
@@ -114,9 +114,9 @@ server <- function(input, output) {
   
   density.files <- eventReactive({input$process}, {
     # remove previous uploads
-    unlink("Data", recursive=TRUE)
+    unlink("Data/*", recursive=TRUE)
     # unzip user data
-    dir.create("Data")
+    dir.create("Data", recursive=TRUE)
     unzip.files <- unzip(input$upload$datapath, list = FALSE, exdir = "Data")
     # get rid of OSX hidden and empty stuff
     density.files <- subset(unzip.files, !grepl("__MACOSX|.DS_Store|/$", unzip.files))
